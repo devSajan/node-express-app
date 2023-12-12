7f9c8fb7874b193c4931516388dfa47184008593
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2023-12-12 18:34:36 +0530

    add testMatch in jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironmentOptions: {
     NODE_ENV: 'test',
   },
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
   restoreMocks: true,
   coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
   coverageReporters: ['text', 'lcov', 'clover', 'html'],
