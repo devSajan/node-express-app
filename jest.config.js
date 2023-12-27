e01af8b264d11c35006ec2cc1808fa83c6273a22
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2023-12-27 19:03:08 +0530

    use jest-html-reports npm module

--- jest.config.js
+++ jest.config.js
@@ -8,18 +8,34 @@ module.exports = {
   restoreMocks: true,
   // coveragePathIgnorePatterns: ['node_modules', 'src/config', 'src/app.js', 'tests'],
   // coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  // !NOTE: jest-html-reporter config
+  // reporters: [
+  //   'default',
+  //   [
+  //     './node_modules/jest-html-reporter',
+  //     {
+  //       pageTitle: 'Test Report',
+  //       includeFailureMsg: true,
+  //       includeSuiteFailure: true,
+  //       includeObsoleteSnapshots: true,
+  //       outputPath: './testResults/test-report.html',
+  //       // includeStackTrace: true,
+  //       // includeConsoleLog: true,
+  //     },
+  //   ],
+  // ],
+  // !NOTE: jest-html-reporters config
   reporters: [
     'default',
     [
-      './node_modules/jest-html-reporter',
+      'jest-html-reporters',
       {
+        publicPath: './test-report',
+        filename: 'report.html',
+        openReport: false,
+        includeConsoleLog: true,
+        darkTheme: true,
         pageTitle: 'Test Report',
-        includeFailureMsg: true,
-        includeSuiteFailure: true,
-        includeObsoleteSnapshots: true,
-        outputPath: './testResults/test-report.html',
-        // includeStackTrace: true,
-        // includeConsoleLog: true,
       },
     ],
   ],
